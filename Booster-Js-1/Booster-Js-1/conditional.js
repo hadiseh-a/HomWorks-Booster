81e9763388e0ac90ec69735c3767b29d030b04bb
hadiseh-a <hadisea82@gmail.com>
2024-08-07 13:20:32 +0330

    js1

---
+++ Booster-Js-1/Booster-Js-1/conditional.js
@@ -0,0 +1,23 @@
+/** @format */
+
+// using if, else if, else write code for below conditions:
+/*
+    if Shaden is a mentor print out "Hi Shaden, you are a mentor"
+    if Shaden is a student print out "Hi Shaden, you are student"
+    if Shaden is a teacher print out "Hi Shaden, you are a teacher"
+    if she is none of above, print out "Hi Shaden, you should join APS"
+*/
+
+let yourName = 'Shaden';
+let yourRole = 'mentor';
+
+//your code here//
+
+
+///
+
+/* 
+EXPECTED RESULT
+---------------
+Hi Shaden, you are a mentor.
+*/
