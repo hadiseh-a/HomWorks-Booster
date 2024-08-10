bfb745bcd55797971c5cbebd2e078dc34ffe49b5
hadiseh-a <hadisea82@gmail.com>
2024-08-10 14:10:34 +0330

    add js3

---
+++ Booster-Js-3/Booster-Js-3/A-array-find/exercise.js
@@ -0,0 +1,30 @@
+/* 
+  You are given an array of names.
+  Write a function called "findLongNameThatStartsWithA" using the .find() method. This function should find the first name in names array that starts with the letter 'A' and is longer than seven letters.
+*/
+
+// write your code here
+
+let names = [
+  "Vajihe",
+  "Alireza",
+  "AmirMostafa",
+  "AmirMehdi",
+  "Alie",
+  "Sarah",
+  "MohammadHosein",
+  "Fatemeh",
+  "Mahjan",
+];
+
+const findLongNameThatStartsWithA = function (names) {
+  return names.find(
+    (name) => (name[0] === "A" || name[0] === "a") && name.length > 7
+  );
+}; // create the function
+
+let longNameThatStartsWithA = findLongNameThatStartsWithA(names); // call the function
+console.log(longNameThatStartsWithA);
+
+/* EXPECTED OUTPUT */
+// "AmirMostafa"
---
+++ Booster-Js-3/Booster-Js-3/F-array-map/array-map.js
@@ -0,0 +1,15 @@
+// Using the .map() method, create a new array with `numbers` multiplied by 100
+// Write multiple solutions using different syntax (as shown in the README)
+
+let numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+const add100 = (num) => {
+ 
+};
+
+let solutionOne = ;
+let solutionTwo = ;
+
+let solutionThree = ;
+
+console.log(solutionOne, solutionTwo, solutionThree);
