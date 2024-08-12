323026a5fb3551e88d41cd90496348bfa8e341a3
hadiseh-a <hadisea82@gmail.com>
2024-08-12 13:01:38 +0330

    add js3

---
+++ Booster-Js-4/basketball.js
@@ -0,0 +1,10 @@
+/*
+ John and Mike both play basketball on different teams. In the latest three games:
+ 
+ 1- John's team scored 89, 120, and 103 points.
+ 2- Mike's team scored 116, 94, and 123 points.
+
+ Calculate the average score for each team.
+ Determine which team wins on average (highest average score), and print the winner to the console. Also, include the average score in the output.
+ Then, change the scores to show different winners. 
+*/
---
+++ Booster-Js-4/bills&tips.js
@@ -0,0 +1,16 @@
+/*
+John and his family went on a holiday and visited three different restaurants. The bills were: $124, $48, $268
+To tip the waiter a fair amount, John created a simple tip calculator as a function. He likes to tip:
+ - 20% of the bill when the bill is less than $50
+ - 15% when the bill is between $50 and $200
+ - 10% if the bill is more than $200
+In the end, John would like to have two arrays:
+
+An array containing all three tips (one for each bill).
+An array containing all three final paid amounts (bill + tip).
+*/
+
+function calculateTip(bill) {}
+
+let tips;
+let totalPaid;
