6483b19f9b409c6643546a146766eabe1e5f3ee3
hadiseh-a <hadisea82@gmail.com>
2024-08-21 18:36:58 +0330

    done js6

--- Booster-Js-6/1-quiz.js
+++ Booster-Js-6/1-quiz.js
@@ -1,5 +1,5 @@
-const apiKey = '';
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=CityName&units=imperial&appid=${apiKey}`;
+const apiKey = "ae17c7d9277b86ebe1d2c58ec8d33533";
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=cityName&units=imperial&appid=${apiKey}`;
 
 /**
    * Create a  forecast weather program to which take a city name an return the weather situation of the city.
@@ -24,3 +24,36 @@ const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=CityName&unit
    * to access to description => data.list[0].weather[0].description
 
    */
+
+//2
+
+function fetchWeather(cityName) {
+  return new Promise((resolve) => {
+    resolve(fetch(apiUrl.replace("cityName", cityName)));
+  });
+}
+
+//3-4
+
+const weatherInfo = document.querySelector("#weather-info");
+
+function setWeatherInfo(data) {
+  weatherInfo.innerHTML = "";
+  const temperature = document.createElement("p");
+  temperature.innerText = `temperature : ${data.list[0].main.temp}`;
+  const weatherDescription = document.createElement("p");
+  weatherDescription.innerText = `weather description : ${data.list[0].weather[0].description}`;
+  const cityName = document.createElement("h1");
+  cityName.innerText = `City : ${data.city.name}`;
+  weatherInfo.append(cityName, temperature, weatherDescription);
+}
+
+fetchWeather("Mashhad")
+  .then((resolve) => resolve)
+  .then((res) => res.json())
+  .then((data) => setWeatherInfo(data))
+  .catch(
+    () =>
+      (weatherInfo.innerText =
+        "Failed to fetch weather information. Please try again later.")
+  );
--- Booster-Js-6/color-changer.js
+++ Booster-Js-6/color-changer.js
@@ -15,3 +15,33 @@ By modifying the code in the text input, the color of the page should update.
 3- In the second section(History Section):
 Display the last 10 colors selected by the user.
 */
+
+const root = document.querySelector("#root");
+root.style.display = "flex";
+root.style.justifyContent = "center";
+
+//first section
+
+const firstSection = document.createElement("section");
+const textInput = document.createElement("input");
+textInput.setAttribute("type", "text");
+const colorInput = document.createElement("input");
+colorInput.setAttribute("type", "color");
+firstSection.append(textInput, colorInput);
+root.append(firstSection);
+
+//second section
+
+const secondSection = document.createElement("section");
+const list = document.createElement("ol");
+secondSection.append(list);
+root.append(secondSection);
+
+firstSection.addEventListener("change", (e) => {
+  textInput.value = e.target.value;
+  document.body.style.backgroundColor = textInput.value;
+  if (list.children.length > 9) {
+    list.children[0].remove();
+    list.innerHTML += `<li>${textInput.value}</li>`;
+  } else list.innerHTML += `<li>${textInput.value}</li>`;
+});
